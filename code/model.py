59a3168a354d870a7d9d61a5f6bd02a3dcc61702
Ricky Ma <mr.rickyma@gmail.com>
2020-11-11 18:49:12 -0800

    Refactored simulation setup into model.py, debugged training

--- code/model.py
+++ code/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import softmax
 
 
 class TD:
@@ -7,42 +8,65 @@ class TD:
     the discount factor, γ, and the bootstrapping factor, λ, may be functions
     of state.
     If that doesn't seem germane to your problem, just use a constant value for them.
-    Attributes
-    ----------
-    n : int
-        The number of features (and therefore the length of the weight vector).
-    z : Vector[float]
-        The eligibility trace vector.
-    w : Vector[float]
-        The weight vector.
     """
-    def __init__(self, n):
+    def __init__(self, subgraphs):
         """Initialize the learning algorithm.
         Parameters
         -----------
-        n : int
-            The number of features, i.e. expected length of the feature vector.
+        subgraphs : List[Graph]
+            List of decomposed graphs.
         """
-        self.n = n
-        self.w = np.zeros(self.n)
-        self.z = np.zeros(self.n)
+
+        '''
+        simulation setup
+        '''
+        self.SUBGRAPHS = subgraphs
+        self.NUM_AGENTS = self.SUBGRAPHS[0].size  # number of agents (size of adj matrix)
+        self.NUM_ACTIONS = 3  # actions per agent
+        self.TOTAL_ACTIONS = int(np.power(self.NUM_ACTIONS, self.NUM_AGENTS))  # total actions
+        self.TOTAL_STATES = 20  # total states
+        self.DIM_PHI = self.TOTAL_ACTIONS * self.TOTAL_STATES  # |S||A|
+        self.DIM_FEATURES = 30  # value of d
+        self.VEC_EPSILON = np.random.rand(self.TOTAL_ACTIONS, 1)
+        self.INIT_STATE = np.random.randint(self.TOTAL_STATES)
+        self.TOTAL_SAMPLES = 1e6
+        self.DISCOUNT = 0.5
+        self.WEIGHT = np.zeros((self.DIM_FEATURES, self.NUM_AGENTS))
+
+        '''
+        sample generation
+        '''
+        # transition matrix
+        self.mtx_prob_ssa = np.ones((self.TOTAL_STATES, self.TOTAL_STATES, self.TOTAL_ACTIONS)) / self.TOTAL_STATES
+        # reward generation
+        self.reward_as = np.random.rand(self.DIM_PHI, self.NUM_AGENTS)
+        # behavior policy
+        self.vec_prob_as = softmax(self.VEC_EPSILON)
+        self.cdf_prob_as = np.cumsum(self.vec_prob_as)
+
+        # self.z = np.mean(self.w, 2)
+
+        '''
+        feature generation
+        '''
+        self.feature = np.cos(np.pi * np.random.rand(self.DIM_FEATURES, self.DIM_PHI))
+        feature_vec_norm = np.linalg.norm(self.feature, ord=2, axis=1)[:, np.newaxis]
+        self.feature = self.feature / feature_vec_norm
 
     def get_value(self, s):
         """Get the approximate value for feature vector `phi`."""
-        return np.dot(self.w, s)
+        return np.dot(self.WEIGHT, s)
 
-    def update(self, subgraph, phi, r, phip, eta):
+    def update(self, subgraph, idx_feature_curr, idx_feature_pred, eta):
         """Update from new experience.
         Parameters
         ----------
         subgraph: Vector[float]
             The n*n mixing matrix from the current timestep.
-        phi : Vector[float]
-            The observation/features from the current timestep.
-        r : float
-            The reward from the transition.
-        phip : Vector[float]
-            The observation/features from the next timestep.
+        idx_feature_curr : int
+            Index of current feature vector.
+        idx_feature_pred : Vector[int]
+            Vector of indices of next feature vector for each agent.
         eta : float
             The step-size parameter for updating the weight vector.
         Returns
@@ -50,18 +74,26 @@ class TD:
         delta : float
             The temporal difference error from the update.
         """
-        temp = self.w + eta * phi * (r + np.dot(phip - phi, self.w))
-        delta = np.zeros(self.w.shape)
-        for row in range(subgraph.size):
+
+        delta = np.zeros(self.WEIGHT.shape)
+
+        for agent in range(subgraph.size):
+            r = self.reward_as[idx_feature_curr, agent]
+            phi = self.feature[:, idx_feature_curr]
+            phip = self.feature[:, idx_feature_pred[agent]]
+            wk = self.WEIGHT[:, agent]
+            temp = wk + eta * phi * (r + np.dot(phip - phi, wk))
+
             for col in range(subgraph.size):
-                b = np.asarray(subgraph.adjMatrix)[row][col]
+                b = np.asarray(subgraph.adjMatrix)[agent][col]
                 if b > 0:
-                    delta += b * temp
-        self.w = delta
-        print(self.w)
+                    delta[:, agent] += b * temp
+
+        self.WEIGHT += delta
+        print(self.WEIGHT)
         return delta
 
-    def reset(self):
-        """Reset weights, traces, and other parameters."""
-        self.w = np.zeros(self.n)
-        self.z = np.zeros(self.n)
+    # def reset(self):
+    #     """Reset weights, traces, and other parameters."""
+    #     self.w = np.zeros(self.n)
+    #     self.z = np.zeros(self.n)
--- code/train.py
+++ code/train.py
@@ -1,93 +1,51 @@
 from code import graph
 from code.model import TD
 import numpy as np
-from numpy import linalg, random
-from scipy.special import softmax
 
-'''
-simulation setup
-'''
-NUM_AGENTS = 10  # number of agents
-NUM_ACTIONS = 3  # actions per agent
-TOTAL_ACTIONS = int(np.power(NUM_ACTIONS, NUM_AGENTS))  # total actions
-TOTAL_STATES = 20  # total states
-DIM_PHI = TOTAL_ACTIONS * TOTAL_STATES  # |S||A|
-DIM_FEATURES = 30  # value of d
-VEC_EPSILON = random.rand(TOTAL_ACTIONS, 1)
-INIT_STATE = random.randint(TOTAL_STATES)
-TOTAL_SAMPLES = 1e6
-STEPSIZE = 0.005
-DISCOUNT = 0.5
-'''
-1. sample generation
-'''
-# 1.1 transition matrix
-mtx_prob_ssa = np.ones((TOTAL_STATES, TOTAL_STATES, TOTAL_ACTIONS))/TOTAL_STATES
-# 1.2 reward generation
-reward_as = random.rand(DIM_PHI, NUM_AGENTS)
-# 1.3 behavior policy
-vec_prob_as = softmax(VEC_EPSILON)
-cdf_prob_as = np.cumsum(vec_prob_as)
-'''
-2. mixing matrix generation
-'''
-mix_matrix, G = graph.gen_graph('ER', 10, 0.7)
-subgraphs = graph.decompose(mix_matrix, G)
+# TODO: implement error tracing
 # ww_star = np.mean(init_weight, 2)
-# 3. feature generation
-feature = np.cos(np.pi * random.rand(DIM_FEATURES, DIM_PHI))
-feature_vec_norm = linalg.norm(feature, ord=2, axis=1)[:, np.newaxis]
-# print(vec_norm.shape)
-# print(np.ones((DIM_FEATURES, 1)).shape)
-# temp = np.ones((DIM_FEATURES, 1)) @ vec_norm.T
-# print(temp)
-# feature = feature / (np.ones((DIM_FEATURES, 1)) @ feature_vec_norm.T)
-feature = feature / feature_vec_norm
 
 
-def train(eta=STEPSIZE, B=subgraphs):
-    # TODO: debug training, matrix/vector multiplications
-    init_state = INIT_STATE
-    model = TD(DIM_FEATURES)
+def train(num_agents, subgraphs, eta=0.005):
+    model = TD(subgraphs)
+    for sample in range(int(model.TOTAL_SAMPLES)):
+        init_state = model.INIT_STATE
+        for k, subgraph in enumerate(subgraphs):
+            print(subgraph)
+            print("subgraph: " + str(k))
 
-    for k, subgraph in enumerate(B):
-        print(subgraph)
-        # model.update(b=subgraph, phi=init_phi, phip=init_phip, r=init_reward, eta=eta)
-        print("subgraph: " + str(k))
+            # find current action, state
+            rnd_tos = np.random.uniform(0, 1)
+            idx_action_curr = np.argwhere(rnd_tos <= model.cdf_prob_as)[0][0]
+            idx_state_curr = init_state
 
-        # find action
-        rnd_tos = random.uniform(0, 1)
-        idx_action_curr = np.argwhere(rnd_tos <= cdf_prob_as)[0][0]
-        idx_state_curr = init_state
+            # find next state
+            cdf_prob_ssa = np.cumsum(model.mtx_prob_ssa[:, idx_state_curr, idx_action_curr])
+            rnd_tos = np.random.uniform(0, 1)
+            idx_state_next = np.argwhere(rnd_tos <= cdf_prob_ssa)[0][0]
 
-        # 4.2 find next state
-        cdf_prob_ssa = np.cumsum(mtx_prob_ssa[:, idx_state_curr, idx_action_curr])
-        rnd_tos = random.uniform(0, 1)
-        idx_state_next = np.argwhere(rnd_tos <= cdf_prob_ssa)[0][0]
+            # find next action
+            range_phi = (idx_state_next - 1) * model.TOTAL_ACTIONS + np.arange(0, model.TOTAL_ACTIONS)
+            idx_action_pred = np.empty(shape=(num_agents, 1))
+            for agent in range(num_agents):
+                idx_action_pred[agent] = np.argmax(model.feature[:, range_phi].conj().T @ model.WEIGHT[:, agent])
 
-        # 4.3 find next action
-        range_phi = (idx_state_next-1) * TOTAL_ACTIONS + np.arange(0, TOTAL_ACTIONS)
-        # idx_action_pred = np.zeros((NUM_AGENTS, 1))
-        # print(range_phi.shape)
-        # print(feature[:, range_phi].conj().T.shape)
-        # for jj in range(NUM_AGENTS):
-        #     print(jj)
-        #     idx_action_pred[jj] = np.argmax(feature[:, range_phi].conj().T @ init_weight[:, jj])
-        idx_action_pred = np.argmax(feature[:, range_phi].conj().T @ model.w)
-        idx_feature_curr = (idx_state_curr-1)*TOTAL_ACTIONS + idx_action_curr
-        idx_feature_pred = (idx_state_next-1)*TOTAL_ACTIONS + idx_action_pred
+            idx_feature_curr = (idx_state_curr - 1) * model.TOTAL_ACTIONS + idx_action_curr
+            idx_feature_pred = (idx_state_next - 1) * model.TOTAL_ACTIONS + idx_action_pred
 
-        # print(idx_feature_curr.shape)
-        # print(idx_feature_curr)
-        # print(idx_action_pred.shape)
-        # print(idx_action_pred)
-        # print(idx_feature_pred.shape)
-        # print(idx_feature_pred)
+            # print(idx_feature_curr.shape)
+            # print(idx_feature_curr)
+            # print(idx_action_pred.shape)
+            # print(idx_action_pred)
+            # print(idx_feature_pred.shape)
+            # print(idx_feature_pred)
 
-        reward = reward_as[idx_feature_curr, k]
-        phi = feature[:, idx_feature_curr.astype(int)]
-        phip = feature[:, idx_feature_pred.astype(int)]
-        model.update(subgraph=subgraph, phi=phi, phip=phip, r=reward, eta=eta)
+            # parameter updates
+            model.update(subgraph=subgraph,
+                         idx_feature_curr=idx_feature_curr,
+                         idx_feature_pred=idx_feature_pred.astype(int),
+                         eta=eta)
+            model.INIT_STATE = idx_state_next
 
 
 if __name__ == '__main__':
@@ -97,4 +55,4 @@ if __name__ == '__main__':
     for i, graph in enumerate(subgraphs):
         print("Color: " + str(i + 1))
         graph.print_matrix()
-    train()
+    train(num_agents=G.size, subgraphs=subgraphs)
